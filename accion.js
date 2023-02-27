72c827a06c757e9e0f888f7be68d5375478eeed5
ZotGabk <110762666+ZotGabk@users.noreply.github.com>
2023-02-27 17:55:40 -0300

    Add files via upload

---
+++ accion.js
@@ -0,0 +1,4 @@
+document.getElementById("boton").onclick = function () {
+    console.log("capturamos el evento click");
+    document.getElementById("demo").innerHTML = "Gracias por enviar la oferta laboral";
+}
